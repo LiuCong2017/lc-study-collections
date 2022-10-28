060edfe5ff3e23636831b92a95480a78960a5ebf
Kavin <2018568647@qq.com>
2022-10-28 16:18:48 +0800

    add new

---
+++ lc-frontend/lc-web/lc-typescript-study/typescript-demo/01-the-basics/demo_01.ts
@@ -0,0 +1,22 @@
+/**
+ *  A type-checker like TypeScript uses that information
+ *  and tells us when things might be going off the rails.
+ */
+// message.toLowerCase();
+// message();
+// const message = "hello";
+
+// function fn(x){
+//     return x.flip();
+// }
+
+// const user = {
+//     name: "kavin",
+//     age: 19,
+// };
+// user.location;
+
+function greet(person: string,date: Date){
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`)
+}
+greet("kavin",new Date());
